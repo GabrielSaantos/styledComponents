41b6325707dfc6b337d0e4e0f17ef60adbd3c521
Gabriel Santos <gabriel.santos@bit7forge.com>
2025-04-21 22:55:56 -0300

    create search module

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,5 +1,5 @@
 import { UserProps } from "../types/user";
-import React from "react";
+
 import Search from "../components/Search";
 import { useState } from "react";
 
@@ -10,10 +10,22 @@ const Home = () => {
     const res = await fetch(`https://api.github.com/users/${userName}`);
 
     const data = await res.json();
+
+    const { avatar_url, login, location, followers, following } = data;
+
+    const userData: UserProps = {
+      avatar_url,
+      login,
+      location,
+      followers,
+      following,
+    };
+    setUser(userData);
   };
   return (
     <div>
       <Search loadUser={loadUser} />
+      {user && <p>{user.login}</p>}
     </div>
   );
 };
